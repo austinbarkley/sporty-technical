023173e33fdc03d3903488b72a051736668b9147
Austin Barkley <barkley.a83@gmail.com>
2025-08-20 13:41:00 +0200

    initial setup and move to npm

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import ListPage from './pages/List/List.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/list',
+    element: <ListPage />,
   },
 ]);
 
